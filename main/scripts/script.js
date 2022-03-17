d7b5d69ffff30aec9313d24cbd60b352ccd2f4f2
Ashutosh Adhikari <89822484+Adhikari-Ashutosh@users.noreply.github.com>
2022-03-17 18:03:13 +0000

    Initial commit

---
+++ main/scripts/script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4dbce9',
+        'primary-dark': '#4338ca',
+				'primary-hover': '#4f42f0',
+      },
+    },
+  },
+};
